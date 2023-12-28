c835811b7e3de3a8769ac834e80628cba3028a97
jcarvajalocares2023 <154937577+jcarvajalocares2023@users.noreply.github.com>
2023-12-28 19:23:15 -0300

    Update GreetingService.java

--- src/main/java/com/example/testingweb/GreetingService.java
+++ src/main/java/com/example/testingweb/GreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class GreetingService {
 	public String greet() {
-		String password ="1234"
+		String password ="1234";
 		return "Hola, soy Jorge Carvajal";
 	}
 }
